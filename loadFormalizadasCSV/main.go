f559ae71bdf4d7ef6f6f211af364af9e20b31061
Jose del Río <joserio@bysidecar.com>
2019-01-24 09:56:33 +0100

    Functionality to load a CSV file (Evo Banco formalizadas list from Salesforce) into a table in our system. The structure of the CSV file (number and type of columns) can't be modified.

---
+++ loadFormalizadasCSV/main.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"database/sql"
+	"dev/mysqllibtest/implementeddb"
+	"dev/readparams"
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
+)
+
+type Env struct {
+	db *sql.DB
+}
+
+func main() {
+	f, err := os.OpenFile("../loadFormalizadasCSV_log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer f.Close()
+
+	log.SetOutput(f)
+
+	previoformalizadas("formalizadas.csv")
+}
+
+func previoformalizadas(filename string) {
+
+	filecsv, ferr := os.Open(filename)
+	if ferr != nil {
+		log.Println(ferr)
+		return
+	}
+	defer filecsv.Close()
+
+	rows, csverr := csv.NewReader(filecsv).ReadAll()
+	if csverr != nil {
+		log.Println(csverr)
+		return
+	}
+
+	// produccion!!!!!!!!3
+	connString := readparams.GetConnString(3)
+	db, conerr := implementeddb.OpenConnection(connString)
+	if conerr != nil {
+		log.Println(conerr)
+		log.Println(db)
+		return
+	}
+
+	env := &Env{db: db}
+
+	//WEBSERVICE PRODUCCIÓN
+	vuelcaFormalizadas(env.db, rows)
+	defer db.Close()
+
+	// report_panel WEBSERVICE!!!!!!!!4
+	connString = readparams.GetConnString(4)
+	db, err := implementeddb.OpenConnection(connString)
+	if err != nil {
+		log.Println(err)
+		log.Println(db)
+		return
+	}
+
+	env = &Env{db: db}
+	vuelcaFormalizadas(env.db, rows)
+
+	defer db.Close()
+}
+
+func vuelcaFormalizadas(db *sql.DB, rows [][]string) {
+
+	altrows := rows[1:]
+
+	sqlTruncate := "delete from webservice.evo_formalizadas_sf_v2 where date(FECHA_FORMALIZACION) >= '2018-12-01';"
+	_, err := db.Query(sqlTruncate)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	sql := "INSERT INTO webservice.evo_formalizadas_sf_v2 (NOMBRE,CLIENTID,NUMERO_PROCESO_CONTRATACION,PRODUCTO,NUMERO_EXPEDIENTE,ID_PERSONA_IRIS,FECHA_FORMALIZACION) VALUES %s"
+	final := make([]string, len(altrows))
+	// var finalArgs []interface{}
+	// finalArgs := make([]interface{}, 0, len(altrows))
+	finalArgs := []interface{}{}
+
+	for z, row := range altrows {
+		r := strings.Split(row[0], ";")
+		t, timerr := time.Parse("02/01/2006 15:04", strings.Trim(r[6], " "))
+		if timerr != nil {
+			log.Println(timerr)
+			log.Println(t)
+			return
+		}
+
+		tam := len(r)
+		valueStrings := make([]string, 0, tam)
+		for i := 0; i < tam; i++ {
+			valueStrings = append(valueStrings, "?")
+		}
+		final[z] = "(" + strings.TrimSuffix(strings.Join(valueStrings, ","), ",") + ")"
+		finalArgs = append(finalArgs, r[0], r[1], r[2], r[3], r[4], r[5], t)
+	}
+
+	stmtStr := fmt.Sprintf(sql, strings.Join(final, ","))
+	stmt, _ := db.Prepare(stmtStr)
+	_, stmterr := stmt.Exec(finalArgs...)
+	if stmterr != nil {
+		log.Println(finalArgs)
+		log.Println(stmterr)
+		return
+	}
+}
