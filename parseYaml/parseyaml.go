15f859358d22afce903ede2aaed825c164191520
Jose del Río <joserio@bysidecar.com>
2019-02-13 11:20:55 +0100

    -Added support for parsing yaml with new structure - Added support for generate struct file from the new yaml structure. Supports two levels of nested arrays in json structure. - Create a folder to store the generated struct in the context from the script is invoked. - Now the generated struct is constructed using template.Funcs method, which is more readable than the templates approach.

--- parseYaml/parseyaml.go
+++ parseYaml/parseyaml.go
@@ -1,6 +1,7 @@
 package utilities
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
@@ -17,18 +18,24 @@ type Fileconfig struct {
 		Extension string `yaml:"extension"`
 	} `yaml:"source"`
 	Proccess struct {
-		Structname  string `yaml:"structname"`
+		Tipo        string `yaml:"type"`
+		Name        string `yaml:"name"`
 		Packagename string `yaml:"packagename"`
+		Estructura  []struct {
+			Field      string       `yaml:"field"`
+			TipoCampo  string       `yaml:"type"`
+			Structname string       `yaml:"structname"`
+			Structure  []properties `yaml:"structure"`
+		} `yaml:"structure"`
 	} `yaml:"proccess"`
 	Destination struct {
-		Tipo           string   `yaml:"type"`
-		Tabla          string   `yaml:"table"`
-		Estructura     []string `yaml:"structure"`
-		EstructuraJSON []struct {
+		Tipo       string `yaml:"type"`
+		Tabla      string `yaml:"table"`
+		Estructura []struct {
 			Field     string `yaml:"field"`
 			Order     int    `yaml:"order"`
 			TipoCampo string `yaml:"type"`
-		} `yaml:"structurejson"`
+		} `yaml:"structure"`
 	} `yaml:"destination"`
 }
 
@@ -45,13 +52,15 @@ type structconfig struct {
 // properties is a struct that represents a property of a Struct Type.
 // Contains a Name (string) and a Type(string) to set the name and the type of the struct property.
 type properties struct {
-	Name string
-	Type string
+	Structname string
+	Type       string
+	Field      string
+	Comillas   string
+	Structure  []properties
 }
 
-//parse function populates a Fileconfig struct with values from a Yaml configuration file provided as param.
-func (f Fileconfig) parse(path string) (Fileconfig, error) {
-
+//Parse function populates a Fileconfig struct with values from a Yaml configuration file path provided as param.
+func (f Fileconfig) Parse(path string) (Fileconfig, error) {
 	file, err := os.Open(path)
 	if err != nil {
 		log.Println(err)
@@ -60,32 +69,174 @@ func (f Fileconfig) parse(path string) (Fileconfig, error) {
 	defer file.Close()
 
 	data, err := ioutil.ReadAll(file)
-	err = yaml.Unmarshal([]byte(data), &f)
 	if err != nil {
 		log.Fatalf("error: %v", err)
 		return f, err
 	}
+
+	err = yaml.Unmarshal([]byte(data), &f)
+	if err != nil {
+		log.Fatalf("error marshaling: %v", err)
+		return f, err
+	}
+
+	fmt.Println("--------- Parse result -----------")
+	fmt.Println(f)
+	fmt.Println("--------- ------------ -----------")
 	return f, nil
+}
+
+func checkStructure(p *properties, array []properties) {
+	if len(array) > 0 {
+		for _, row := range array {
+			subp := properties{
+				Structname: row.Structname,
+				Type:       row.Type,
+				Field:      row.Field,
+				Comillas:   "a",
+			}
+
+			if len(row.Structure) > 0 {
+				b := subcheckStructure(row.Structure)
+				subp.Structure = b
+			}
+			p.Structure = append(p.Structure, subp)
+		}
+	}
+}
+
+func subcheckStructure(array []properties) []properties {
+	list := make([]properties, 0)
+	for _, r := range array {
+		subp := properties{
+			Structname: r.Structname,
+			Type:       r.Type,
+			Field:      r.Field,
+			Comillas:   "`",
+		}
+		if len(r.Structure) > 0 {
+			a := subcheckStructure(r.Structure)
+			subp.Structure = a
+		}
+		list = append(list, subp)
+	}
+	return list
 }
 
 // GenerateStruct is a public function that generates a Struct type dinamically.
 // Uses a Fileconfig struct populated with parse method and creates a .go file with the properties and names provided in the config file.
 func (f Fileconfig) GenerateStruct(pathconfig string) {
 
-	fc, err := f.parse(pathconfig)
+	fc, err := f.Parse(pathconfig)
 	if err != nil {
 		log.Println(err)
 	}
 
 	sc := structconfig{
 		Packagename: fc.Proccess.Packagename,
-		Namestruct:  fc.Proccess.Structname,
+		Namestruct:  fc.Proccess.Name,
 		Property:    make([]properties, 0),
 	}
 
-	for _, jsonRow := range fc.Destination.EstructuraJSON {
-		sc.Property = append(sc.Property, properties{Name: jsonRow.Field, Type: jsonRow.TipoCampo})
+	for _, jsonRow := range fc.Proccess.Estructura {
+		p := properties{
+			Structname: jsonRow.Structname,
+			Type:       jsonRow.TipoCampo,
+			Field:      jsonRow.Field,
+			Comillas:   "`",
+		}
+		checkStructure(&p, jsonRow.Structure)
+		sc.Property = append(sc.Property, p)
 	}
+	fmt.Println("--------- Structconfig result -----------")
+	fmt.Println(sc)
+	fmt.Println("--------- ------------ -----------")
+
+	funcMap := template.FuncMap{
+		"testFunc": func(array structconfig) string {
+			a := ""
+			a += "package " + array.Packagename
+			a += "\n"
+			a += "type " + array.Namestruct + " struct { "
+			a += "\n"
+
+			for _, k := range array.Property {
+
+				if len(k.Structure) > 0 {
+					for _, row := range k.Structure {
+						a += row.Structname + " " + row.Type + "{"
+
+						if len(row.Structure) > 0 {
+							for _, r := range row.Structure {
+								a += r.Structname + " " + r.Type + " " + r.Comillas + "json:\"" + r.Field + "\"" + r.Comillas + "\n"
+							}
+						}
+						a += "}`json:\"" + row.Field + "\"`\n"
+					}
+				} else {
+					a += k.Structname + " " + k.Type + " " + k.Comillas + "json:\"" + k.Field + "\"" + k.Comillas
+					a += "\n"
+				}
+			}
+			a += " }"
+			return a
+		},
+	}
+
+	var testTemplate = `
+		{{ testFunc . }}
+	`
+	templ, ferr := template.New("test").Funcs(funcMap).Parse(testTemplate)
+	err = templ.Execute(os.Stdout, sc)
+
+	if _, err := os.Stat("./generatedStructs/"); !os.IsNotExist(err) {
+		// path does not exist
+		os.Mkdir("./generatedStructs/", 0755)
+	}
+
+	file, ferr := os.Create("./generatedStructs/" + fc.Proccess.Name + ".go")
+	if ferr != nil {
+		log.Println(ferr)
+		return
+	}
+	defer file.Close()
+
+	perr := templ.Execute(file, sc)
+	if perr != nil {
+		log.Println(perr)
+	}
+}
+
+// GenerateStruct is a public function that generates a Struct type dinamically.
+// Uses a Fileconfig struct populated with parse method and creates a .go file with the properties and names provided in the config file.
+// In this case text templates are used to print the final values.
+// Not use
+func (f Fileconfig) withTemplates(pathconfig string) {
+
+	fc, err := f.Parse(pathconfig)
+	if err != nil {
+		log.Println(err)
+	}
+
+	sc := structconfig{
+		Packagename: fc.Proccess.Packagename,
+		Namestruct:  fc.Proccess.Name,
+		Property:    make([]properties, 0),
+	}
+
+	for _, jsonRow := range fc.Proccess.Estructura {
+		p := properties{
+			Structname: jsonRow.Structname,
+			Type:       jsonRow.TipoCampo,
+			Field:      jsonRow.Field,
+			Comillas:   "`",
+		}
+		checkStructure(&p, jsonRow.Structure)
+		sc.Property = append(sc.Property, p)
+	}
+	fmt.Println("--------- Structconfig result -----------")
+	fmt.Println(sc)
+	fmt.Println("--------- ------------ -----------")
 
 	var structTemplate = `
 		package {{.Packagename}}
@@ -94,7 +245,15 @@ func (f Fileconfig) GenerateStruct(pathconfig string) {
 	`
 	var keyvalueTemplate = `
 		{{range $y, $x := .Property }}
-		{{ $x.Name }}  {{ $x.Type }}
+			{{if .Structure}}
+				{{ $x.Structname }}  {{ $x.Type }} {
+				{{range $yy, $xx := .Structure }}
+					{{ $xx.Structname }}  {{ $xx.Type }} {{ $xx.Comillas }}json:"{{ $xx.Field }}"{{ $xx.Comillas }}
+				{{end}}
+				}  {{ $x.Comillas }}json:"{{ $x.Field }}"{{ $x.Comillas }}
+			{{else}}
+				{{ $x.Structname }}  {{ $x.Type }}  {{ $x.Comillas }}json:"{{ $x.Field }}"{{ $x.Comillas }}
+			{{end}}
 		{{end}}
 	`
 
@@ -106,15 +265,7 @@ func (f Fileconfig) GenerateStruct(pathconfig string) {
 	addChildTemplate(parent, structTemp)
 	addChildTemplate(parent, keyvalueTemp)
 
-	// file, ferr := os.Create("./generatedStructs/" + f.Proccess.Structname + ".go")
-	file, ferr := os.Create(fc.Proccess.Structname + ".go")
-	if ferr != nil {
-		log.Println(ferr)
-		return
-	}
-	defer file.Close()
-
-	perr := parent.Execute(file, sc)
+	perr := parent.Execute(os.Stdout, sc)
 	if perr != nil {
 		log.Println(perr)
 	}
